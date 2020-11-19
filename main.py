1cfbc160b2880773a4269d953ea724d427e94b96
gotimo2 <61739017+gotimo2@users.noreply.github.com>
2020-11-19 10:22:40 +0100

    comments toegevoegd

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from tkinter import * #pylint:disable = unused-wildcard-import
+from tkinter import * #pylint:disable=unused-wildcard-import
 import json
 
+#steam.json importen als dictionary
 jsonFile = open('steam.json')
 steamDictionary = json.load(jsonFile)
 jsonFile.close()
+
+#maak window
 window = Tk()
 
+
+window.configure(
+    
+)
+#run window
 window.mainloop()
