6e6db5fe5c97a5df647b94cd59f27e24c0330cc1
David <74706230+daviddiamantis@users.noreply.github.com>
2021-01-11 14:10:27 +0100

    Code voor status toegevoegd, verandert nog niet dat gaat afstandssensor doen onder 'ti component codes'

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from tkinter import ttk
 import steam_games
 from steam_games import *  # pylint:disable=unused-wildcard-import
 import time
-
+#import RPI.GPIO as GPIO
+#GPIO.setmode(GPIO.BCM)
 # maak window en maak een lokale """kopie""" van listofgames voor makkelijk gebruik en manipulatie
 root = Tk()
 root.title("Steam Tool") #zet titel van window naar "Steam Tool"
 listOfGames = steam_games.listOfGames
 
-
-def raise_frame(frame): 
+def raise_frame(frame):
     frame.tkraise()
-
 
 #maak frames
 f1 = Frame(root)
@@ -21,33 +20,24 @@ f2 = Frame(root)
 f3 = Frame(root)
 f4 = Frame(root)
 
-
 #refereer naar de frames als "news"
 for frame in (f1,f2, f3, f4):
     frame.grid(row=0, column=0, ipady=40, ipadx=35, sticky='news')
 
-
 #maak canvas
 C = Canvas(master=root, bg="blue", height=250, width=300)
-
 #neem photoimage voor later?
 filename = PhotoImage(file="steam image3.png")
-
 #maak labels
-
 background_label = Label(master=f1, image=filename)
 background_label2 = Label(master=f2, image=filename)
-
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 background_label2.place(x=0, y=0, relwidth=1, relheight=1)
-
 background_label3 = Label(master=f3, image=filename)
 background_label3.place(x=0, y=0, relwidth=1, relheight=1)
-
 background_label4 = Label(master=f4, image=filename)
 background_label4.place(x=0, y=0, relwidth=1, relheight=1)
 C.pack
-
 
 #maak label voor naam?
 MenuLabel = Label(f1, text='Steam Tool', font=('Helvetica', 12, 'bold italic'), height=2, width=20)
@@ -60,32 +50,69 @@ vriendenlijst = Button(f1, text='Vriendenlijst', command=lambda: raise_frame(f4)
 for i in [overzichtgames, statistieken, vriendenlijst]:
     i.pack(pady=10)
 
+# status weergave
+def status_circle(x, y, r, icon_status):
+    x0 = x - r
+    y0 = y - r
+    x1 = x + r
+    y1 = y + r
+    # if status == 'online': #Checkt wat de status is en past kleur hierop aan (status zelf moet nog geimplenteerd worden)
+    #     kleur = 'green'
+    # elif status == 'away':
+    #     kleur = 'yellow'
+    # elif status == 'offline':
+    #     kleur = 'white'
+    return icon_status.create_oval(x0, y0, x1, y1, fill='green')
+
+#gebruiker status canvas
+CanvasStatus = Canvas(f1)
+CanvasStatus.pack(side=BOTTOM,  anchor=SE)
+CanvasStatus2 = Canvas(f2)
+CanvasStatus2.pack(side=BOTTOM, anchor=SE)
+CanvasStatus3= Canvas(f3)
+CanvasStatus3.pack(side=BOTTOM, anchor=SE)
+CanvasStatus4 = Canvas(f4)
+CanvasStatus4.pack(side=BOTTOM, anchor=SE)
+
+#roept status_circle() aan
+icon1 = (CanvasStatus)
+icoon1 = status_circle(70, 20, 7, icon1)
+icon2 = (CanvasStatus2)
+icoon2 = status_circle(70, 20, 7, icon2)
+icon3 = (CanvasStatus3)
+icoon3 = status_circle(70, 20, 7, icon3)
+icon4 = (CanvasStatus4)
+icoon4 = status_circle(70, 20, 7, icon4)
+#gebruiker status labels
+StatusGebruiker = Label(CanvasStatus, text= 'Status:')
+StatusGebruiker.pack(side=BOTTOM, anchor=SE,pady=10,padx=20)
+StatusGebruiker2 = Label(CanvasStatus2, text= 'Status:')
+StatusGebruiker2.pack(side=BOTTOM, anchor=SE,pady=10,padx=20)
+StatusGebruiker3 = Label(CanvasStatus3, text= 'Status:')
+StatusGebruiker3.pack(side=BOTTOM, anchor=SE,pady=10,padx=20)
+StatusGebruiker4 = Label(CanvasStatus4, text= 'Status:')
+StatusGebruiker4.pack(side=BOTTOM, anchor=SE,pady=10,padx=20)
+
 #headingfuncties -Tedieus, uitgebreid, alleen nodig want tkinter commands.
 #doen wat ze moeten doen tho ¯\_(ツ)_/¯
 def name_heading():
     sortByName()
     refreshGames()
-
 def rating_heading():
     sortByRating()
     refreshGames()
-
 def price_heading():
     sortByPrice()
     refreshGames()
-
 def age_heading():
     sortByAge()
     refreshGames()
-
 def releaseDateHeading():
     sortByReleaseDate()
     refreshGames()
-
 def appidHeading():
     sortByAppid()
     refreshGames()
-
 #launch game functie
 def launchGame():
     treeSelected = tree.focus() #pak gefocuste item van de treeview
@@ -93,18 +120,14 @@ def launchGame():
     currentid = valueList[2][5] #pak de steamid ervan
     #print(valueList[2][5]) #print de steamid
     os.system(f"start \"\" steam://run/{currentid}") #open de steam://run voor de gekozen game
-
 def reverseList():
     listOfGames.reverse()
     refreshGames()
-
-
 
 #maak tree
 #maak tree met scrollbar
 tree_scroll=Scrollbar(f2)
 tree_scroll.pack(side=RIGHT,fill=Y)
-
 tree = ttk.Treeview(f2,yscrollcommand=tree_scroll.set, column=("column1", "column2", "column3","column4", "column5", "column6"), show='headings', height=25)
 tree_scroll.configure(command=tree.yview)
 #configureer tree
@@ -116,11 +139,8 @@ tree.heading("#5", text="Uitkomstdatum", command=releaseDateHeading)
 tree.heading("#6", text="AppID", command=appidHeading)
 
 
-
-
 #plaats tree
 tree.pack(pady=10, padx=10)
-
 ##knoppen om te sorteren, --moeten naar heading veranderd worden-- zijn nu naar heading veranderd, dus onnodig. ik hou ze hier gewoon voor het geval dat.
 #Button(f2, text='Sorteer op naam', command=sortByName).pack(pady=10)
 #Button(f2, text='Sorteer games op uitkomstdatum', command=sortByReleaseDate).pack(pady=10)
@@ -128,27 +148,20 @@ tree.pack(pady=10, padx=10)
 #Button(f2, text='Sorteer games op prijs', command=sortByPrice).pack(pady=10)
 #Button(f2, text='Sorteer games op Waardering', command=sortByRating).pack(pady=10)
 #Button(f2, text='Sorteer games op datum AppID', command=sortByAppid).pack(pady=10)
-
 #knop voor steam launch en lijst omdraaien
 Button(f2, text='Start game', command=launchGame).pack(pady=10, side=BOTTOM)
 Button(f2, text = 'Lijst omkeren', command=reverseList).pack(pady=10)
-
 #knoppen en labels voor welkom en teruggaan
 Button(f2, text='Terug', command=lambda: raise_frame(f1)).pack(pady=10)
-
 Label(f3, text='Welkom', font=('Helvetica', 12, 'bold italic'), height=2, width=20).pack()
 Button(f3, text='Terug', command=lambda: raise_frame(f1)).pack(pady=10)
-
 Label(f4, text='Welkom', font=('Helvetica', 12, 'bold italic'), height=2, width=20).pack()
 Button(f4, text='Terug', command=lambda: raise_frame(f1)).pack(pady=10)
-
-
 
 def refreshGames():
     tree.delete(*tree.get_children()) #leeg de tree
     for i in listOfGames: #plaats de list opnieuw
         tree.insert(parent='', index='end', iid=i.appid, text="game", values=(i.name, round(i.rating, 2), i.price, i.required_age, i.release_date, i.appid))
-
 
 refreshGames()
 # log de games in de huidige volgorde naar log.txt zod
@@ -158,17 +171,20 @@ def logGames():
             file.write(
                 f'{i.name}: {i.price}, {i.release_date} - {round(i.rating, 1)}% positive reviews, ({round(i.required_age, 0)}+) - product {i.appid}\n')
 
-
 sortByAppid()
-
 #print(listOfGames[0].name)
-
 
 #def refreshLabelLoop():
     #root.after(50, refreshLabelLoop)
 
+"""""
+-----------------------------------------------------------
+                                TI COMPONENT CODES
+-----------------------------------------------------------
+"""""
+#def AfstandSensor(): #Checkt of gebruiker achter pc zit en past de status aan.
+
 
 # run window
 raise_frame(f1)
-
 root.mainloop()
